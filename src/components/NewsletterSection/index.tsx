122282734d7b692436b8d5c175fbb85379608348
GaldinoMat <mateusgaldino1000@hotmail.com>
2023-04-25 23:36:08 -0300

    feat: implement newsletter section functionality

--- src/components/NewsletterSection/index.tsx
+++ src/components/NewsletterSection/index.tsx
@@ -1,18 +1,40 @@
+import { toast } from "react-toastify";
+
 function NewsletterSection() {
   return (
     <section className="mt-10 bg-[#FAFAFA] py-6 flex flex-col items-center gap-2">
       <h4 className="font-bold text-[#353535] text-2xl text-center">
         Recebe Nossa Newsletter
       </h4>
-      <form className="flex gap-4">
+      <form
+        className="flex gap-4"
+        onSubmit={(e) => {
+          e.preventDefault();
+
+          toast.success("Obrigado por se cadastrar!", {
+            position: "bottom-left",
+            autoClose: 5000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+        }}
+      >
         <input
           className="border border-black rounded pl-6 w-[12.75rem] text-[#353535] placeholder:text-[#353535]"
           type="email"
           name=""
           id=""
           placeholder="Digite seu e-mail"
+          required
         />
-        <button className="text-sm font-bold bg-[#FAA500] w-[4.75rem] text-center rounded py-1 text-white">
+        <button
+          type="submit"
+          className="text-sm font-bold bg-[#FAA500] w-[4.75rem] text-center rounded py-1 text-white"
+        >
           Enviar
         </button>
       </form>
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,25 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import Layout from "./components/Layout/index.tsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Layout>
       <App />
+      <ToastContainer
+        position="bottom-left"
+        autoClose={5000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </Layout>
   </React.StrictMode>
 );
